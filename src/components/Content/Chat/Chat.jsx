543456c83659eaf53cf8aa4103e1f5da54a45be5
riddikV <vovak1772@gmail.com>
2024-02-10 19:22:56 +0300

    chat

--- src/components/Content/Chat/Chat.jsx
+++ src/components/Content/Chat/Chat.jsx
@@ -2,9 +2,27 @@ import cls from './Chat.module.css';
 
 const Chat = () => {
 	return (
-		<>
-			<h1 className='hell'>Chat</h1>
-		</>
+		<div className={cls.dialogs}>
+			<div className={cls.dialogs__user}>
+				
+				<div className={cls.chat__header}>
+					<div className={cls.chat__header_title}>
+						<h2>Чат</h2>
+					</div>
+
+					<div className={cls.chat__header_i_w}>
+						<div className={cls.chat__header_input}>
+							<input type="text" placeholder='text'/>
+						</div>
+					</div>
+				</div>
+
+				<div className={cls.chat__chats}>fasdfasdf</div>
+			</div>
+
+
+			<div className={cls.dialogs__chat}>chat</div>
+		</div>
 	)
 }
 
